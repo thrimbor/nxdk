4b201dba12dd800cec241557f8744a6841ac16d9
Stefan Schmidt <thrimbor.github@gmail.com>
2020-03-15 15:54:22 +0100

    net: Reformat Pktdrv code

--- lib/net/pktdrv/pktdrv.h
+++ lib/net/pktdrv/pktdrv.h
@@ -1,17 +1,17 @@
-#ifndef _Pktdrv_
-#define _Pktdrv_
+#ifndef __PKTDRV_H__
+#define __PKTDRV_H__
 
 #ifdef __cplusplus
 extern "C" {
 #endif
 
-extern void* PktdrvMmioBase;
+extern void *PktdrvMmioBase;
 extern unsigned int PktdrvInterrupt;
 
 int Pktdrv_Init(void);
 void Pktdrv_Quit(void);
 int Pktdrv_ReceivePackets(void);
-void Pktdrv_SendPacket(unsigned char *buffer,int length);
+void Pktdrv_SendPacket(unsigned char *buffer, int length);
 void Pktdrv_GetEthernetAddr(unsigned char *address);
 int Pktdrv_GetQueuedTxPkts(void);
 
