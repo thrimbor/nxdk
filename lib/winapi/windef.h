fd31b8c082cc46225fcd42e0d1111dbfaeaff94e
Stefan Schmidt <thrimbor.github@gmail.com>
2021-10-29 08:33:11 +0200

    implement macros

--- lib/winapi/windef.h
+++ lib/winapi/windef.h
@@ -18,4 +18,11 @@ typedef HINSTANCE HMODULE;
 
 typedef int (FAR WINAPI *FARPROC)(void);
 
+#define MAKELONG(wLow, wHigh) ((((DWORD)((wHigh) & 0xffff)) << 16) | ((wLow) & 0xffff))
+#define HIWORD(dwValue) ((WORD)((wValue) >> 16) & 0xffff)
+#define LOWORD(dwValue) ((WORD)(wValue) & 0xffff)
+#define MAKEWORD(bLow, bHigh) ((((WORD)((bHigh) & 0xff)) << 8) | ((bLow) & 0xff))
+#define HIBYTE(wValue) ((BYTE)((wValue) >> 8) & 0xff)
+#define LOBYTE(wValue) ((BYTE)(wValue) & 0xff)
+
 #endif
