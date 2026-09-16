718e6c0bb079412e6e2a3a098bc5b561b64c83f3
Stefan Schmidt <thrimbor.github@gmail.com>
2026-09-17 01:16:04 +0200

    libc: Define max_align_t

---
+++ lib/xboxrt/libc_extensions/stddef_ext_.h
@@ -0,0 +1,7 @@
+// SPDX-License-Identifier: MIT
+
+// SPDX-FileCopyrightText: 2026 Stefan Schmidt
+
+#if ((defined(__cplusplus) && __cplusplus >= 201103L) || (defined(__STDC_VERSION__) && __STDC_VERSION__ >= 201112L))
+typedef double max_align_t;
+#endif
